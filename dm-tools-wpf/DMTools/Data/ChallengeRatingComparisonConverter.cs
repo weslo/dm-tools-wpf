cd2562c727e5e7edf652c6204b3d0de58a5bbc33
Wes Rockholz <wesley.rockholz@gmail.com>
2018-04-15 20:25:58 -0400

    Display warnings for monsters with a CR that exceeds the average player level of the party

---
+++ dm-tools-wpf/DMTools/Data/ChallengeRatingComparisonConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace DMTools.Data
+{
+    // Converter for comparing challenge rating to average party level.
+    public class ChallengeRatingComparisonConverter : IMultiValueConverter
+    {
+        // Return true if the challenge rating is too dangerous for the party level.
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Convert challenge rating to max safe level.
+            string cr = values[0] as string;
+            double crValue = Utilities.Math.ParseFraction(cr);
+            int maxLevel = (int)Math.Floor(crValue);
+
+            // Get average party level from parameter.
+            int partyLevel = (int)values[1];
+            return maxLevel > partyLevel;
+        }
+
+        // Cannot convert back.
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- dm-tools-wpf/DMTools/Utilities/Math.cs
+++ dm-tools-wpf/DMTools/Utilities/Math.cs
@@ -18,5 +18,34 @@ namespace DMTools.Utilities
             }
             return value;
         }
+
+        // Parse a double from a fraction string.
+        public static double ParseFraction(string fractionString)
+        {
+            // Try parsing a normal double first.
+            if(double.TryParse(fractionString, out double result))
+            {
+                return result;
+            }
+
+            // Split the string on the forward slash.
+            string[] split = fractionString.Split('/');
+
+            // Handle improperly formatted fractions.
+            if(split.Length != 2)
+            {
+                throw new FormatException("Input string must be in the format 'a/b'.");
+            }
+
+            // Parse parameters.
+            if(double.TryParse(split[0], out double a) && double.TryParse(split[1], out double b))
+            {
+                return a / b;
+            }
+            else
+            {
+                throw new FormatException("In input string 'a/b', a and b must be valid doubles.");
+            }
+        }
     }
 }
